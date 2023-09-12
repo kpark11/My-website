130eb3408cf4f0f5cc8a805514de5bb8b78ce3de
Kiman Park <kimanpark33@gmail.com>
2023-09-12 15:59:58 -0400

    adding pictures

--- Our-website.py
+++ Our-website.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
+# In[7]:
 
 
 ### This program for Kiman and Abby to utilize the internet space for jobs, projects, and data visualizations. ###
@@ -24,7 +24,9 @@ import matplotlib.pyplot as plt
 import base64
 import dash_bootstrap_components as dbc
 
-image_path = 'assets/Kiman-Abby.jpeg'
+image_path = './assets/Kiman-Abby.jpeg'
+
+
 
 app = dash.Dash(external_stylesheets=[dbc.themes.LUX])
 
@@ -39,7 +41,7 @@ app.layout = html.Div([
     html.H2("Our Story:", style={'textAlign': 'center', 'color': '#FF8903'}),
     html.P("This is our story! We have so much to show and tell!", 
            style={'textAlign':'center'}),
-    html.Img(src=image_path,
+    html.Img(src=app.get_asset_url('my-image.png'),
         style={'width': 500, #'98%''
                 'height': 400, #'60px'
                 'borderRadius': "5px",
