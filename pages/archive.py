401195c89114674f27b26cf6a63a2fa9a88a5334
Kiman Park <kimanpark33@gmail.com>
2023-09-12 17:29:56 -0400

    adding multiple pages

---
+++ pages/archive.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import dash
+from dash import html
+
+dash.register_page(__name__)
+
+layout = html.Div([
+    html.H1('This is our Archive page'),
+    html.Div('This is our Archive page content.'),
+])
+
