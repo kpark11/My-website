daa718aee624cd925a21de9c17c4f5682af8ea23
Kiman Park <kimanpark33@gmail.com>
2023-09-12 16:44:38 -0400

    trying to add pictures

--- Our-website.py
+++ Our-website.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[18]:
+# In[22]:
 
 
 ### This program for Kiman and Abby to utilize the internet space for jobs, projects, and data visualizations. ###
@@ -28,6 +28,10 @@ import dash_bootstrap_components as dbc
 image_path = 'https://github.com/kpark11/Our-website/tree/main/assets/Kiman-Abby.jpeg'
 
 cwd = os.getcwd()
+lists = os.listdir(cwd)
+lists1 = os.listdir('/opt/render/project')
+lists2 = os.listdir('/opt/render')
+lists3 = os.listdir('/opt')
 
 app = dash.Dash(external_stylesheets=[dbc.themes.LUX])
 
@@ -42,7 +46,7 @@ app.layout = html.Div([
     html.H2("Our Story:", style={'textAlign': 'center', 'color': '#FF8903'}),
     html.P("This is our story! We have so much to show and tell!", 
            style={'textAlign':'center'}),
-    html.Img(src=app.get_asset_url('Kiman-Abby.jpeg'),
+    html.Img(src=image_path,#app.get_asset_url('Kiman-Abby.jpeg'),
         style={'width': 500, #'98%''
                 'height': 400, #'60px'
                 'borderRadius': "5px",
@@ -51,7 +55,11 @@ app.layout = html.Div([
                'display': 'block',
               'margin-left': 'auto',
               'margin-right': 'auto'}),
-    html.P(cwd)
+    html.P(cwd),
+    html.P(lists),
+    html.P(lists1),
+    html.P(lists2),
+    html.P(lists3)
 ])
 
 
