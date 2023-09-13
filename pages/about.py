dba9083b5bb2029c5311afd3a593a47b0b76a07f
Kiman Park <kimanpark33@gmail.com>
2023-09-13 14:51:11 -0400

    Trying to add multiple pages

--- pages/about.py
+++ pages/about.py
@@ -12,11 +12,13 @@ from dash import dcc,State,html
 dash.register_page(__name__)
 
 layout = html.Div([
-        html.H2("About us:"),
-        html.P('Kiman Park: Oak Ridge National Laboratory - Postdoctoral Research Associate'),
-        dcc.Link('kimanpark33@gmail.com'),
+        html.H2("About us:",style={'textAlign': 'center', 'color': '#FF8903'}),
+        html.P('Kiman Park:',style={'textAlign': 'center', 'color': '#FF8903'}),
+        html.P('Oak Ridge National Laboratory - Postdoctoral Research Associate',style={'textAlign': 'center', 'color': '#FF8903'}),
+        dcc.Link('kimanpark33@gmail.com',style={'textAlign': 'center', 'color': '#FF8903'}),
         html.br(),
-        html.P('Abby Carpenter: Tennessee Department of Health - Epidemiologist'),
-        dcc.Link('carpenter.abby25@gmail.com')
+        html.P('Abby Carpenter:',style={'textAlign': 'center', 'color': '#FF8903'}),
+        html.P('Tennessee Department of Health - Epidemiologist',style={'textAlign': 'center', 'color': '#FF8903'}),
+        dcc.Link('carpenter.abby25@gmail.com',style={'textAlign': 'center', 'color': '#FF8903'})
         
 ])
