c8d4d7389933eb2da285f3c4713985e379646f03
Kiman Park <kimanpark33@gmail.com>
2023-09-13 14:51:49 -0400

    Trying to add multiple pages

--- pages/about.py
+++ pages/about.py
@@ -13,11 +13,11 @@ dash.register_page(__name__)
 
 layout = html.Div([
         html.H2("About us:",style={'textAlign': 'center', 'color': '#FF8903'}),
-        html.P('Kiman Park:',style={'textAlign': 'center', 'color': '#FF8903'}),
+        html.P('Kiman Park, Ph.D.:',style={'textAlign': 'center', 'color': '#FF8903'}),
         html.P('Oak Ridge National Laboratory - Postdoctoral Research Associate',style={'textAlign': 'center', 'color': '#FF8903'}),
         dcc.Link('kimanpark33@gmail.com',style={'textAlign': 'center', 'color': '#FF8903'}),
         html.br(),
-        html.P('Abby Carpenter:',style={'textAlign': 'center', 'color': '#FF8903'}),
+        html.P('Abby Carpenter, MPH:',style={'textAlign': 'center', 'color': '#FF8903'}),
         html.P('Tennessee Department of Health - Epidemiologist',style={'textAlign': 'center', 'color': '#FF8903'}),
         dcc.Link('carpenter.abby25@gmail.com',style={'textAlign': 'center', 'color': '#FF8903'})
         
