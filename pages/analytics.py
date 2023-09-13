a4fe6b32f190aa8e6bc112df35f7e50afa251d03
Kiman Park <kimanpark33@gmail.com>
2023-09-13 14:20:21 -0400

    Trying to add multiple pages

--- pages/analytics.py
+++ pages/analytics.py
@@ -7,7 +7,7 @@ from dash import html, dcc, callback, Input, Output
 
 dash.register_page(__name__)
 
-layout = html.Div([
+app.layout = html.Div([
     html.H1('This is our Analytics page'),
     html.Div([
         "Select a city: ",
--- pages/archive.py
+++ pages/archive.py
@@ -7,7 +7,7 @@ from dash import html
 
 dash.register_page(__name__)
 
-layout = html.Div([
+app.layout = html.Div([
     html.H1('This is our Archive page'),
     html.Div('This is our Archive page content.'),
 ])
