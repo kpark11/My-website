8122866407a455a66d8dfc51893376bd2a4b5bc6
Kiman Park <kimanpark33@gmail.com>
2023-09-14 12:22:52 -0400

    making side by side paragraphs

--- pages/about.py
+++ pages/about.py
@@ -18,25 +18,7 @@ layout = html.Div([
         
         
          html.Div([
-           html.Div(children=[
-               
-               html.P('Kiman Park, Ph.D.:',
-                       style={'textAlign': 'center','font-weight': 'bold'}),
-                html.P('Oak Ridge National Laboratory - Postdoctoral Research Associate',
-                       style={'textAlign': 'center'}),
-                html.P(dcc.Link(href='kimanpark33@gmail.com'),
-                       style={'textAlign': 'center'}),
-                    ],
-                        style={
-                        #'backgroundColor':'darkslategray',
-                        #'color':'lightsteelblue',
-                        #'height':'100px',
-                        #'margin-left':'10px',
-                        'width':'49%',
-                        'text-align':'center',
-                        'display':'inline-block'
-                        }),
-
+           
             html.Div(children=[
                 
                 html.P('Abby Carpenter, MPH:',
@@ -56,6 +38,25 @@ layout = html.Div([
                         'display':'inline-block'
                }),
                 ]),
+         
+         html.Div(children=[
+               
+               html.P('Kiman Park, Ph.D.:',
+                       style={'textAlign': 'center','font-weight': 'bold'}),
+                html.P('Oak Ridge National Laboratory - Postdoctoral Research Associate',
+                       style={'textAlign': 'center'}),
+                html.P(dcc.Link(href='kimanpark33@gmail.com'),
+                       style={'textAlign': 'center'}),
+                    ],
+                        style={
+                        #'backgroundColor':'darkslategray',
+                        #'color':'lightsteelblue',
+                        #'height':'100px',
+                        #'margin-left':'10px',
+                        'width':'49%',
+                        'text-align':'center',
+                        'display':'inline-block'
+                        }),
         
         
         
