41011b73ca66b6d1caea0ae030681205886824fe
Kiman Park <kimanpark33@gmail.com>
2023-09-25 14:16:11 -0400

    added a sentence about Coursera

--- pages/analytics.py
+++ pages/analytics.py
@@ -34,8 +34,8 @@ year_list = [i for i in range(1980, 2024, 1)]
 
 layout = html.Div([
     #TASK 2.1 Add title to the dashboard
-    html.H1("Automobile Statistics Dashboard"),#May include style for title
-    html.P("This is from the Coursera exercise."),
+    html.H1("Automobile Statistics Dashboard",style={'textAlign': 'center', 'color': '#FF8903'}),#May include style for title
+    html.P("This is from the Coursera exercise.",style={'textAlign':'center'}),
     html.Div([#TASK 2.2: Add two dropdown menus
         html.Label("Select Statistics:"),
         dcc.Dropdown(
