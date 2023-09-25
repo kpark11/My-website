5040ecc835f8c6e92660dca7324fb4434b918725
Kiman Park <kimanpark33@gmail.com>
2023-09-25 13:43:21 -0400

    added a sentence about Coursera

--- pages/analytics.py
+++ pages/analytics.py
@@ -8,7 +8,6 @@ import dash
 from dash import html, dcc
 import pandas as pd
 from dash.dependencies import Input, Output
-import pandas as pd
 import plotly.graph_objs as go
 import plotly.express as px
 
