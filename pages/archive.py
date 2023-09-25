d435d09fe27f525a242fe49d2bc7b1cdc49c04ef
Kiman Park <kimanpark33@gmail.com>
2023-09-25 14:19:13 -0400

    added a sentence about Coursera

--- pages/archive.py
+++ pages/archive.py
@@ -9,6 +9,9 @@ dash.register_page(__name__)
 
 layout = html.Div([
     html.H2('This is our Archive page',style={'textAlign': 'center', 'color': '#FF8903'}),
-    html.Div('This is our Archive page content.'),
+    html.Div('This is our Archive page content.',style={'textAlign':'center'}),
+    html.Div('Go to the github page:'),
+    html.Div(html.P(dcc.Link(href='https://github.com/kpark11'),
+                       style={'textAlign': 'center'}),)
 ])
 
