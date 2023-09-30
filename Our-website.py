1527d2a33eed6ac1a4b1fe0db5e2f8d328ff6db4
Kiman Park <kimanpark33@gmail.com>
2023-09-30 16:56:59 -0400

    trying to add nested pages into a folder

--- Our-website.py
+++ Our-website.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
+# In[21]:
 
 
 ### This program for Kiman and Abby to utilize the internet space for jobs, projects, and data visualizations. ###
@@ -13,18 +13,33 @@ from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 import os
 
-cwd = os.getcwd()
-directors = os.listdir()
+image_path = 'https://github.com/kpark11/Our-website/blob/main/assets/statistics.webp?raw=true'
 
-app = dash.Dash(external_stylesheets=[dbc.themes.LUX],pages_folder="/opt/render/project/src/pages")
+app = dash.Dash(external_stylesheets=[dbc.themes.LUX])#,pages_folder="/opt/render/project/src/pages")
 
-server = app.server
+#server = app.server
 
 app.title = "Kiman and Abby Park"
 app.style = {'textAlign':'center','color':'#503D36','font-size':24}
 #---------------------------------------------------------------------------------
 
-app.layout = html.Div([style={'backgroundColor':'#D4FFFF'},
+app.layout = html.Div([
+    
+    html.Div([html.Img(src=image_path,
+    style={
+          'verticalAlign':'middle',
+          'textAlign': 'center',
+          #'background-image':image,
+          'position':'fixed',
+          'width':'100%',
+          'height':'100%',
+          'top':'0px',
+          'left':'0px',
+          'z-index':'1000',
+          'opacity': '0.2'
+            })
+    ]),
+    
     html.H1("Kiman and Abby Park", style={'textAlign': 'center', 'color': '#3E57B0','font-size':50}), 
     html.P(print(cwd)),
     html.P(print(directors)),
@@ -32,11 +47,9 @@ app.layout = html.Div([style={'backgroundColor':'#D4FFFF'},
             html.Div(
                 dcc.Link(f"{page['name']}", href=page["path"])
                 ) for page in dash.page_registry.values() if not page["path"].startswith("/projects")
-        ]),
+                ]),
             dash.page_container
     ])
-    
-
     
     
 if __name__ == '__main__':
