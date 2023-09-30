360e8f741eea303937c221e261d428f398f584af
Kiman Park <kimanpark33@gmail.com>
2023-09-30 16:41:17 -0400

    trying to add nested pages into a folder

--- Our-website.py
+++ Our-website.py
@@ -25,7 +25,7 @@ app.style = {'textAlign':'center','color':'#503D36','font-size':24}
 #---------------------------------------------------------------------------------
 
 app.layout = html.Div([
-    html.H1("Kiman and Abby Park", style={'textAlign': 'center', 'color': '#3E57B0','font-size':50,'backgroundColor':'blue'}), 
+    html.H1("Kiman and Abby Park", style={'textAlign': 'center', 'color': '#3E57B0','font-size':50}), 
     html.P(print(cwd)),
     html.P(print(directors)),
         html.Div([
@@ -34,7 +34,8 @@ app.layout = html.Div([
                 ) for page in dash.page_registry.values() if not page["path"].startswith("/projects")
         ]),
             dash.page_container
-    ])
+    ]),
+    style={'backgroundColor':'#D4FFFF'},
 
     
     
