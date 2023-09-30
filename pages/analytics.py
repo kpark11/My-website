85ae3a43d58cca56d805ee9bd7111fdd384ab4e8
Kiman Park <kimanpark33@gmail.com>
2023-09-29 21:06:55 -0400

    trying to add pages within pages

--- pages/analytics.py
+++ pages/analytics.py
@@ -13,17 +13,10 @@ layout = html.Div([
     html.H2('This is our Projects: ',style={'textAlign': 'center', 'color': '#FF8903'}),
     html.Br(),
     html.Br(),
-    html.Br(),
-    html.Br(),
-    html.Br(),
-    html.Br(),
-    html.Br(),
-    html.Br(),
     html.Div([
         html.Div(
-             dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
+             dcc.Link(page['name'], href=page["relative_path"])
                 ) for page in dash.page_registry.values() 
         if page["path"].startswith("/projects")
-    ]),
-    dash.page_container
+    ])
 ])
