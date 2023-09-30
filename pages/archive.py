83b06e6f39aaf7cc6ef73b3cf69b574fb1a572b4
Kiman Park <kimanpark33@gmail.com>
2023-09-29 23:33:41 -0400

    trying to add pages within pages

--- pages/archive.py
+++ pages/archive.py
@@ -9,7 +9,9 @@ dash.register_page(__name__,order=3)
 
 layout = html.Div([
     html.H2('This is our Archive page',style={'textAlign': 'center', 'color': '#FF8903'}),
-    html.Div('This is our Archive page content.',style={'textAlign':'center'}),
+    html.Br(),
+    html.Div('Our archive in in the github.',style={'textAlign':'center'}),
+    html.Br(),
     html.Div('Go to the github page:',style={'textAlign':'center'}),
     html.Div(html.P(dcc.Link(href='https://github.com/kpark11'),
                        style={'textAlign': 'center'}),)
