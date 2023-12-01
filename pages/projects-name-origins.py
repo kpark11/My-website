4e123172c2e3dbc24f3408cefa8ac979cf026ea0
Kiman Park <kimanpark33@gmail.com>
2023-12-01 17:22:01 -0500

    modified

--- pages/projects-name-origins.py
+++ pages/projects-name-origins.py
@@ -22,7 +22,6 @@ import math
 import requests
 from bs4 import BeautifulSoup
 import json
-import pip
 
 
 
@@ -306,7 +305,7 @@ def update_output(n_clicks,val_hidden, val_rate,val_iter):
         if iter % print_every == 0:
             guess, guess_i = categoryFromOutput(output)
             correct = '✓' if guess == category else '✗ (%s)' % category
-            print('%d %d%% (%s) %.4f %s / %s %s' % (iter, iter / n_iters * 100, timeSince(start), loss, line, guess, correct))
+            return print('%d %d%% (%s) %.4f %s / %s %s' % (iter, iter / n_iters * 100, timeSince(start), loss, line, guess, correct))
     
         # Add current loss avg to list of losses
         if iter % plot_every == 0:
