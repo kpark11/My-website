e2d475380dd93379cf7a70ff1881349059896052
Kiman Park <kimanpark33@gmail.com>
2025-02-02 17:47:11 -0500

    modified

--- pages/publications.py
+++ pages/publications.py
@@ -14,6 +14,71 @@ layout = html.Div([
     html.H2('Publications:', className = 'ta archive-header'),
     html.Br(),
     
+    dcc.Markdown(
+            [
+                """
+            <dccLink href="https://pubs.acs.org/journal/inocaj">
+                Magnetically-driven quantum phase transitions in a low-dimensional pyrazine-bridged Cu<sup>2+</sup> chain magnet
+            </dccLink>
+            <div>
+                Inorganic Chemistry, (Submitted) January 1, 2025
+            </div>
+            
+            
+            <dccLink href="https://pubs.aip.org/aip/apl/article-abstract/122/18/182902/2887973/Spin-phonon-interactions-and-magnetoelectric?redirectedFrom=PDF">
+                Spin-phonon interactions and magnetoelectric coupling in Co<sub>4</sub>B<sub>2</sub>O<sub>9</sub> (B = Nb, Ta)
+            </dccLink>
+            <div>
+                Inorganic Chemistry, (Submitted) January 1, 2025
+            </div>
+            
+            <dccLink href="https://www.nature.com/articles/s41535-022-00438-6">
+                Nonreciprocal directional dichroism at telecom wavelengths
+            </dccLink>
+            <div>
+                npj Quantum Materials, April 4, 2022
+            </div>
+            
+            
+            <dccLink href="https://www.nature.com/articles/s41535-022-00436-8">
+                Vibrational fingerprints of ferroelectric HfO<sub>2</sub>
+            </dccLink>
+            <div>
+                npj Quantum Materials, March 18, 2022
+            </div>
+            
+            
+            <dccLink href="https://journals.aps.org/prb/abstract/10.1103/PhysRevB.104.195143">
+                Band-Mott mixing hybridizes the gap in Fe<sub>2</sub>Mo<sub>3</sub>O<sub>8</sub>
+            </dccLink>
+            <div>
+                Physical Review B, November 22, 2021
+            </div>
+            
+            
+            <dccLink href="https://pubs.rsc.org/en/content/articlelanding/2019/na/c8na00093j">
+                Jolly green MOF: confinement and photoactivation of photosystem I in the metal-organic framework ZIF-8
+            </dccLink>
+            <div>
+                Nanoscale Advances, October 11, 2018
+            </div>
+            
+            
+            <dccLink href="https://pubs.rsc.org/en/content/articlelanding/2017/cy/c7cy00073a">
+                A facile and surfactant-free route for nanomanufacturing of tailored ternary nanoalloys as superior oxygen reduction reaction electrocatalysts
+            </dccLink>
+            <div>
+                Catalysis Science & Technology, April 6, 2017
+            </div>
+            
+            
+            
+                """
+            ],
+            dangerously_allow_html=True,
+        ),
+    
+    '''
     dcc.Link(dcc.Markdown('Magnetically-driven quantum phase transitions in a low-dimensional pyrazine-bridged Cu<sup>2+</sup> chain magnet'),
              href='https://pubs.acs.org/journal/inocaj'),
     html.Div('Inorganic Chemistry, (Submitted) January 1, 2025'),
@@ -36,4 +101,6 @@ layout = html.Div([
     dcc.Link('A facile and surfactant-free route for nanomanufacturing of tailored ternary nanoalloys as superior oxygen reduction reaction electrocatalysts',
               href='https://pubs.rsc.org/en/content/articlelanding/2017/cy/c7cy00073a'),
     html.Div('Catalysis Science & Technology, April 6, 2017'),
+    '''
+    
 ], className = 'ta')
