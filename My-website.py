3126f8125ae046bd961f3b6a2bb028089a6cfa3e
Kiman Park <kimanpark33@gmail.com>
2025-02-02 15:40:09 -0500

    modified

--- My-website.py
+++ My-website.py
@@ -12,7 +12,7 @@ import dash_bootstrap_components as dbc
 import os
 
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX],pages_folder="/opt/render/project/src/pages")
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX], pages_folder="/opt/render/project/src/pages")
 
 server = app.server
 
