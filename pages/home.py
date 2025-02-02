65251ec07018e32d7f9769a49f40df177001046f
Kiman Park <kimanpark33@gmail.com>
2025-02-02 14:57:04 -0500

    modified

--- pages/home.py
+++ pages/home.py
@@ -12,15 +12,14 @@ dash.register_page(__name__, path='/',order=0)
 image_path = 'https://github.com/kpark11/Our-website/blob/main/assets/Kiman.jpeg?raw=true'
 
 layout = html.Div([
-        html.H2("Who I am:", className = 'ta archive-header'),
+    html.H2("Who I am:", className = 'ta archive-header'),
     html.P("""Highly analytical data scientist with a strong foundation in physics, 
            statistics, machine learning, and software development. 
            Proven track record in leveraging advanced computational methods to solve complex problems 
            and deliver impactful solutions. Experienced in collaborating with cross-functional teams to develop 
            innovative data-driven applications. Committed to continuous learning and staying updated with 
-           the latest advancements in data science and technology.""", 
-           className='ta'),
+           the latest advancements in data science and technology.""", ),
     html.Img(src=image_path,#app.get_asset_url('Kiman-Abby.jpeg'),
-        className='image'),
-], className = 'ta-left')
+             className='image'),
+], className = 'ta')
 
