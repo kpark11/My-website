94264c93d412842ac4c5271751e3356ea5db461f
Kiman Park <kimanpark33@gmail.com>
2025-02-02 17:59:40 -0500

    modified

--- pages/publications.py
+++ pages/publications.py
@@ -29,7 +29,7 @@ layout = html.Div([
                 Spin-phonon interactions and magnetoelectric coupling in Co<sub>4</sub>B<sub>2</sub>O<sub>9</sub> (B = Nb, Ta)
             </dccLink>
             <div>
-                Inorganic Chemistry, (Submitted) January 1, 2025
+                Applied Physics Letters, May 2, 2023
             </div>
             
             <dccLink href="https://www.nature.com/articles/s41535-022-00438-6">
@@ -78,29 +78,34 @@ layout = html.Div([
             dangerously_allow_html=True,
         ),
     
-    '''
-    dcc.Link(dcc.Markdown('Magnetically-driven quantum phase transitions in a low-dimensional pyrazine-bridged Cu<sup>2+</sup> chain magnet'),
-             href='https://pubs.acs.org/journal/inocaj'),
-    html.Div('Inorganic Chemistry, (Submitted) January 1, 2025'),
-    dcc.Link(dcc.Markdown('Spin-phonon interactions and magnetoelectric coupling in Co<sub>4</sub>B<sub>2</sub>O<sub>9</sub> (B = Nb, Ta)'),
-             href='https://pubs.aip.org/aip/apl/article-abstract/122/18/182902/2887973/Spin-phonon-interactions-and-magnetoelectric?redirectedFrom=PDF', 
-             className = 'ta'),
-    html.Div('Applied Physics Letters, May 2, 2023'),
-    dcc.Link('Nonreciprocal directional dichroism at telecom wavelengths',
-              href='https://www.nature.com/articles/s41535-022-00438-6'),
-    html.Div('npj Quantum Materials, April 4, 2022'),
-    dcc.Link(dcc.Markdown('Vibrational fingerprints of ferroelectric HfO<sub>2</sub>'),
-              href='https://www.nature.com/articles/s41535-022-00436-8'),
-    html.Div('npj Quantum Materials, March 18, 2022'),
-    dcc.Link(dcc.Markdown('Band-Mott mixing hybridizes the gap in Fe<sub>2</sub>Mo<sub>3</sub>O<sub>8</sub>'), 
-              href='https://journals.aps.org/prb/abstract/10.1103/PhysRevB.104.195143'),
-    html.Div('Physical Review B, November 22, 2021'),
-    dcc.Link('Jolly green MOF: confinement and photoactivation of photosystem I in the metal-organic framework ZIF-8',
-              href='https://pubs.rsc.org/en/content/articlelanding/2019/na/c8na00093j'),
-    html.Div('Nanoscale Advances, October 11, 2018'),
-    dcc.Link('A facile and surfactant-free route for nanomanufacturing of tailored ternary nanoalloys as superior oxygen reduction reaction electrocatalysts',
-              href='https://pubs.rsc.org/en/content/articlelanding/2017/cy/c7cy00073a'),
-    html.Div('Catalysis Science & Technology, April 6, 2017'),
-    '''
+    
     
 ], className = 'ta')
+
+
+
+'''
+dcc.Link(dcc.Markdown('Magnetically-driven quantum phase transitions in a low-dimensional pyrazine-bridged Cu<sup>2+</sup> chain magnet'),
+         href='https://pubs.acs.org/journal/inocaj'),
+html.Div('Inorganic Chemistry, (Submitted) January 1, 2025'),
+dcc.Link(dcc.Markdown('Spin-phonon interactions and magnetoelectric coupling in Co<sub>4</sub>B<sub>2</sub>O<sub>9</sub> (B = Nb, Ta)'),
+         href='https://pubs.aip.org/aip/apl/article-abstract/122/18/182902/2887973/Spin-phonon-interactions-and-magnetoelectric?redirectedFrom=PDF', 
+         className = 'ta'),
+html.Div('Applied Physics Letters, May 2, 2023'),
+dcc.Link('Nonreciprocal directional dichroism at telecom wavelengths',
+          href='https://www.nature.com/articles/s41535-022-00438-6'),
+html.Div('npj Quantum Materials, April 4, 2022'),
+dcc.Link(dcc.Markdown('Vibrational fingerprints of ferroelectric HfO<sub>2</sub>'),
+          href='https://www.nature.com/articles/s41535-022-00436-8'),
+html.Div('npj Quantum Materials, March 18, 2022'),
+dcc.Link(dcc.Markdown('Band-Mott mixing hybridizes the gap in Fe<sub>2</sub>Mo<sub>3</sub>O<sub>8</sub>'), 
+          href='https://journals.aps.org/prb/abstract/10.1103/PhysRevB.104.195143'),
+html.Div('Physical Review B, November 22, 2021'),
+dcc.Link('Jolly green MOF: confinement and photoactivation of photosystem I in the metal-organic framework ZIF-8',
+          href='https://pubs.rsc.org/en/content/articlelanding/2019/na/c8na00093j'),
+html.Div('Nanoscale Advances, October 11, 2018'),
+dcc.Link('A facile and surfactant-free route for nanomanufacturing of tailored ternary nanoalloys as superior oxygen reduction reaction electrocatalysts',
+          href='https://pubs.rsc.org/en/content/articlelanding/2017/cy/c7cy00073a'),
+html.Div('Catalysis Science & Technology, April 6, 2017'),
+'''
+
