8ade7947a7995b1e4252f662b7bc9158f036722d
Kiman Park <kimanpark33@gmail.com>
2025-02-02 22:43:30 -0500

    modified

--- pages/about.py
+++ pages/about.py
@@ -35,10 +35,12 @@ layout = html.Div([
                 
                 dcc.Markdown('**Education:**', className = 'bf underline'),
                 
-                dcc.Markdown('**A Doctor of Philosophy (Ph.D.)** in Physical Chemistry'),
+                dcc.Markdown('**A Doctor of Philosophy (Ph.D.)** in Physical Chemistry', 
+                             dangerously_allow_html=True,),
                 dcc.Markdown('University of Tennessee, May 2018 – May 2023'), 
                 
-                dcc.Markdown('**A Bachelor of Science (B.S.)** in Chemistry'),
+                dcc.Markdown('**A Bachelor of Science (B.S.)** in Chemistry', 
+                             dangerously_allow_html=True,),
                 dcc.Markdown('University of Tennessee, Aug 2014 – May 2018'),                 
                 
                 html.Br(),
